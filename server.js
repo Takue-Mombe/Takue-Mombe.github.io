5839eb986383e984d9ecd21b9a505fb97a2a4339
Takue <h220462g@hit.ac.zw>
2025-03-13 16:56:34 +0200

    tk

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the public directory
-app.use(express.static('public'));
+// Serve static files from the current directory
+app.use(express.static(__dirname));
 
 // Serve index.html for all routes (SPA support)
 app.get('*', (req, res) => {
-    res.sendFile(join(__dirname, 'public', 'index.html'));
+    res.sendFile(join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
